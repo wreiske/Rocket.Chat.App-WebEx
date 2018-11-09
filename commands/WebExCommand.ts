831a17c0b8806a09f51c82472f0c24b2a02b7073
Will Reiske <wreiske@mieweb.com>
2018-11-09 16:12:42 -0500

    Update for 0.0.2

--- commands/WebExCommand.ts
+++ commands/WebExCommand.ts
@@ -1,4 +1,6 @@
 import { IHttp, IModify, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { IMessageAttachment } from '@rocket.chat/apps-engine/definition/messages';
+import { MessageActionType } from '@rocket.chat/apps-engine/definition/messages/MessageActionType';
 import { ISlashCommand, SlashCommandContext } from '@rocket.chat/apps-engine/definition/slashcommands';
 
 export class WebExCommand implements ISlashCommand {
@@ -10,23 +12,40 @@ export class WebExCommand implements ISlashCommand {
     public async executor(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp): Promise<void> {
         const icon = await read.getEnvironmentReader().getSettings().getValueById('webex_icon');
         const company = await read.getEnvironmentReader().getSettings().getValueById('webex_company');
-        
-        //NOTE: there is a bug with mobile Rocket.Chat showing "Full Name" instead of "nfull" for the username
-        //NOTE: this may generate https://company.webex.com/join/Full Name which is NOT a valid link!
+        const username = await read.getEnvironmentReader().getSettings().getValueById('webex_name');
+        const messageSender = await read.getUserReader().getById(context.getSender().id);
 
-        var meetingUrl = "";
-        if(context.getArguments().length >= 1){
+        if (company === '') {
+            const builderError = modify.getCreator().startMessage()
+                .setSender(/* botUser || */ context.getSender()).setRoom(context.getRoom())
+                .setText('Your administrator needs to set a WebEX company in the WebEx App.').setUsernameAlias(username).setAvatarUrl(icon);
+            await modify.getNotifier().notifyUser(context.getSender(), builderError.getMessage());
+            return;
+        }
+
+        let meetingUrl = '';
+        if (context.getArguments().length >= 1) {
             meetingUrl = `https://${company}.webex.com/join/${context.getArguments()[0].trim()}`;
         } else {
-            meetingUrl = `https://${company}.webex.com/join/${context.getSender().username}`;
+            meetingUrl = `https://${company}.webex.com/join/${messageSender.username}`;
         }
-        const text = `@${context.getSender().username} has started a WebEx Meeting: ${meetingUrl}`;
-        const username = await read.getEnvironmentReader().getSettings().getValueById('webex_name');
 
+        // NOTE: there is a bug with mobile Rocket.Chat showing "Full Name" instead of "nfull" for the username
+        // NOTE: this may generate https://company.webex.com/join/Full Name which is NOT a valid link!
+
+        const text = `@${messageSender.username} has started a WebEx Meeting: ${meetingUrl}`;
+
+        const joinButton: IMessageAttachment = {
+            actions: [{
+                type: MessageActionType.BUTTON,
+                text: 'Join Meeting',
+                url: meetingUrl,
+            }],
+        };
         const builder = modify.getCreator().startMessage()
             .setSender(/* botUser || */ context.getSender()).setRoom(context.getRoom())
-            .setText(text).setUsernameAlias(username).setAvatarUrl(icon);
-            
+            .setText(text).setUsernameAlias(username).setAvatarUrl(icon).setAttachments([joinButton]);
+
         await modify.getCreator().finish(builder);
     }
 }
